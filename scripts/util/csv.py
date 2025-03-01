45a055ab6bd6fe212daa193f5661445c9d6abe56
Anton-219 <anton-219@use.startmail.com>
2025-03-01 22:18:19 +0100

    Fix calculation to match with official results

--- scripts/util/csv.py
+++ scripts/util/csv.py
@@ -26,8 +26,6 @@ _age_mapping = {
 _state_mapping = {
     'BB': 'Brandenburg',
     'BE': 'Berlin',
-    'BE-O': 'Berlin',
-    'BE-W': 'Berlin',
     'BW': 'Baden-Württemberg',
     'BY': 'Bayern',
     'HB': 'Bremen',
@@ -62,13 +60,13 @@ def combine_parties_to_other(df: pd.DataFrame, remaining_parties: list[str],
             remain as separate columns.
     :param meta_information_columns: (list[str], optional) A list of column names that
             contain meta-information (e.g., 'Land', 'Stimmenart'). Defaults to
-            ['Land', 'Stimmenart', 'Bezirksart', 'Wahlberechtigte'].
+            ['Land', 'Stimmenart', 'Bezirksart', 'gültig'].
     :return: A DataFrame with the specified parties and meta-information
             columns unchanged, and a new 'Other' column containing the sum of votes
             from all non-specified parties.
     """
     if meta_information_columns is None:
-        meta_information_columns = ['Land', 'Stimmenart', 'Bezirksart', 'Wahlberechtigte']
+        meta_information_columns = ['Land', 'Stimmenart', 'Bezirksart', 'gültig']
     party_cols = df.drop(columns=meta_information_columns)
 
     # Determine which party columns to combine into 'Other'
@@ -85,25 +83,36 @@ def combine_parties_to_other(df: pd.DataFrame, remaining_parties: list[str],
 def combine_vote_results(df_method: pd.DataFrame, df_demographic: pd.DataFrame, vote_type: str) \
         -> list[VoteEntry]:
     df_demographic = df_demographic.copy()
-    available_parties = [col for col in df_demographic.columns
-                         if col not in ['Land', 'Geschlecht', 'Geburtsjahresgruppe', 'Summe', 'Sonstige']]
-    df_demographic[available_parties] = df_demographic[available_parties].div(df_demographic['Summe'], axis=0)
     df_demographic.rename(columns={'Sonstige': 'Other'}, inplace=True)
     df_demographic.fillna(0, inplace=True)
+    available_parties = [col for col in df_demographic.columns
+                         if col not in ['Land', 'Geschlecht', 'Geburtsjahresgruppe', 'Summe']]
 
     entries = []
-    for index, row in df_demographic.iterrows():
-        state = _state_mapping[row['Land']]
+    for index, by_demographic in df_demographic.iterrows():
+        if by_demographic['Land'] not in _state_mapping:
+            print(f"{by_demographic['Land']=} not found in state mapping. Skipping it")
+            continue
+        state = _state_mapping[by_demographic['Land']]
         for _, by_state in df_method[df_method['Land'] == state].iterrows():
             for party in available_parties:
+                total_votes = df_demographic[df_demographic['Land'] == by_demographic['Land']][party].sum()
                 entry = VoteEntry(
                     state=state,
-                    gender=row['Geschlecht'],
-                    ageGroup=row['Geburtsjahresgruppe'],
-                    party=party,
+                    gender=by_demographic['Geschlecht'],
+                    ageGroup=by_demographic['Geburtsjahresgruppe'],
+                    party="Sonstige" if party == "Other" else party,
                     voteType=vote_type,
                     electionMethod=by_state['Bezirksart'],
-                    votes=int(round(row[party] * by_state[party], 0))
+                    votes=calculate_votes(by_demographic, by_state, party, total_votes)
                 )
                 entries.append(entry)
     return entries
+
+
+def calculate_votes(by_demographic, by_state, party, total_votes):
+    if total_votes == 0:
+        return 0
+    return by_demographic[party] * by_state[party] / total_votes
+
+
