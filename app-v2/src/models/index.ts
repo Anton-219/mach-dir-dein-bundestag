dae05838531fd6a9c62703bfb7d3f4a12007ef42
Anton <43760202+Anton-219@users.noreply.github.com>
2026-07-29 01:12:02 +0200

    feat: export election model contracts

---
+++ app-v2/src/models/index.ts
@@ -0,0 +1,12 @@
+export type {
+  AgeGroup,
+  DirectMandateWinner,
+  ElectionMethod,
+  Gender,
+  Party,
+  StatVotes,
+  VoteEntry,
+  VoteType,
+} from './json-contracts.ts'
+
+export type { ElectionResult, SeatResult } from './calculation-results.ts'
