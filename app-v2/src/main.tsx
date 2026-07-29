7ea4d587752241bb2b0b3615748bc3e123c8ffc9
Anton <43760202+Anton-219@users.noreply.github.com>
2026-07-29 21:55:03 +0200

    style: use consolidated filter styles

--- app-v2/src/main.tsx
+++ app-v2/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles.css'
 import './filter-styles.css'
-import './direct-filter-styles.css'
 
 const rootElement = document.getElementById('root')
 
