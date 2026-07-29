6dd1f9a9869d4e0db10d3a4dafac422745317017
Anton <43760202+Anton-219@users.noreply.github.com>
2026-07-29 21:38:11 +0200

    style: load direct filter layout

--- app-v2/src/main.tsx
+++ app-v2/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles.css'
 import './filter-styles.css'
+import './direct-filter-styles.css'
 
 const rootElement = document.getElementById('root')
 
