c95e67fc71fb68db2c2f0c3228a0e2c37dc624fe
Anton <43760202+Anton-219@users.noreply.github.com>
2026-08-08 14:28:26 +0200

    Merge Pages base path with license output

--- vite.config.ts
+++ vite.config.ts
@@ -55,6 +55,7 @@ function localizedDefaultMetadata(): Plugin {
 }
 
 export default defineConfig({
+  base: '/mach-dir-dein-bundestag/',
   build: {
     license: {
       fileName: 'third-party-licenses.md',
